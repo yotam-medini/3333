8586c92197309851865858d2492aab3b8f31721b
Yotam Medini <yotam.medini@gmail.com>
2024-05-14 22:10:10 +0300

    wscon.ts - better late than

---
+++ client/src/wscon.ts
@@ -0,0 +1,91 @@
+import { TableStatus, CallBackIdx } from "./consts";
+import * as c from "./cs_consts";
+console.log("S3333_C2S_NTBL=" + c.S3333_C2S_NTBL);
+
+let E3333_OK = 0;
+
+let win_hostname = window.location.hostname
+if (win_hostname == "") { win_hostname = "localhost"; }
+var host = "ws://" + win_hostname + ":" + "9090";
+console.log("host="+host)
+export let web_socket = new WebSocket(host);
+console.log("web_socket: ", web_socket);
+
+console.log("readyState=" + web_socket.readyState);
+web_socket.onopen = function () {
+  console.log("websocket.onopen");
+  getTablesStatus();
+};
+
+// Callbacks:
+let callbacks = []
+for (let i = 0; i < CallBackIdx.ISIZE; ++i) {
+  callbacks[i] = undefined;
+}
+export let set_callback = function (cbi: CallBackIdx, fn) {
+  console.log("set_callback(" + cbi + ")");
+  callbacks[cbi] = fn;
+}
+
+export let cb_updateTableStatus = undefined;
+
+
+web_socket.onmessage = function (event) {
+  console.log("event.data=" + event.data);
+  let edata = JSON.parse(event.data);
+  let rc = edata['rc'];
+  console.log("edata="+edata+", rc="+rc);
+  if (rc == E3333_OK) {
+    let cmd = edata['cmd'];
+    console.log("cmd="+cmd);
+    let fn = undefined;
+    switch (cmd) {
+      case c.E3333_S2C_NTBL:
+        fn = callbacks[CallBackIdx.ITableStatus];
+	console.log("fn?=" + (fn !== undefined));
+        if (fn !== undefined) {
+	  fn(TableStatus.Own);
+	}
+        break;
+      case c.E3333_S2C_GSTATE:
+        console.log("c.E3333_S2C_GSTATE");
+	let result = edata['result'];
+	let active = result['active'];
+        fn = callbacks[CallBackIdx.ISetCards];
+	console.log("active=" + active + " fn?=" + (fn !== undefined));
+        if (fn !== undefined) {
+	  fn(active);
+	}
+	break;
+      default:
+        console.log("Error: unsupported cmd: " + cmd);
+    }
+  }
+}
+console.log("web_socket: ", web_socket);
+
+export function getTablesStatus() {
+  console.log("web_socket: ", web_socket);
+  web_socket.send("tbls");
+}
+
+export function sendNewTable(
+    table_name: string,
+    table_password: string,
+    player_password: string ) {
+  console.log("sendNewTable");
+  let request = [
+    c.S3333_C2S_NTBL, table_name, 3, table_password, player_password].join(" ")
+  console.log("request="+request);
+  web_socket.send(request);
+}
+
+export function sendNewGame() {
+  web_socket.send(c.S3333_C2S_GNEW);
+}  
+
+export function setTry3(caards_selected: number[3]) {
+  let request = [c.S3333_C2S_TRY3, caards_selected].join(" ")
+  console.log("request="+request);
+  web_socket.send(request);
+}
