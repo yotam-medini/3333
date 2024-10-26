4d2a4be6f085467a457f98a354ee3b02d9dc1d0b
Yotam Medini <yotam.medini@gmail.com>
2024-10-26 22:37:18 +0300

    golden under construction 2

--- qlient/golden.cpp
+++ qlient/golden.cpp
@@ -41,7 +41,7 @@ unsigned Golden::GetNumRows() const {
 using au2_t = std::array<unsigned, 2>;
 
 static int check_disjoint(const Golden& golden) {
-  int rc;
+  int rc = 0;
   const au2_t card_size = golden.GetCardSize();
   const unsigned num_cards = golden.GetNumCards();
   const unsigned num_cols = golden.GetNumColumns();
@@ -50,22 +50,40 @@ static int check_disjoint(const Golden& golden) {
     unsigned col = ci % num_cols;
     unsigned row = ci / num_cols;
     if (col > 0) {
-      const au2_t pos_pre = golden.GetCardPosition(ci - 1);
+      const unsigned pci = ci - 1;
+      const au2_t pos_pre = golden.GetCardPosition(pci);
       if (pos_pre[1] != pos[1]) {
         rc = 1;
-        std::cerr << fmt::format("Heights of cards {} {} differ\n", ci-1, ci);
+        std::cerr << fmt::format("Y-pos of cards {} {} differ\n", pci, ci);
       } else if (pos_pre[0] + card_size[0] >= pos[0]) {
-        std::cerr << fmt::format("cards {} {} intersect\n", ci-1, ci);
+        rc = 1;
+        std::cerr << fmt::format("cards {} {} intersect\n", pci, ci);
+      }
+    }
+    if (row > 0) {
+      const unsigned pci = ci - num_cols;
+      const au2_t pos_pre = golden.GetCardPosition(pci);
+      if (pos_pre[0] != pos[0]) {
+        rc = 1;
+        std::cerr << fmt::format("X-pos of cards {} {} differ\n", pci, ci);
+      } else if (pos_pre[1] + card_size[1] >= pos[1]) {
+        rc = 1;
+        std::cerr << fmt::format("cards {} {} intersect\n", pci, ci);
       }
     }
   }
   return rc;
 }
 
-static int test_golden(unsigned w, unsigned h, unsigned num_cards) {
+static int check_pick(const Golden &golden) {
   int rc = 0;
+  return rc;
+}
+
+static int test_golden(unsigned w, unsigned h, unsigned num_cards) {
   Golden golden(w, h, num_cards);
-  rc = check_disjoint(golden);
+  int rc = check_disjoint(golden);
+  rc = rc ? : check_pick(golden);
   return rc;
 }
 
@@ -80,7 +98,7 @@ int main(int argc, char **argv) {
     rc = 1;
   } else {
     rc = test_golden(
-      std::stoi(argv[0]), std::stoi(argv[1]), std::stoi(argv[2]));
+      std::stoi(argv[1]), std::stoi(argv[2]), std::stoi(argv[3]));
   }
   return rc;
 }
