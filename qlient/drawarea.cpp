debf32e95b2ca342cad9937c5780acc59a507e82
Yotam Medini <yotam.medini@gmail.com>
2024-10-27 13:39:15 +0200

    drawarea under construction, draw_symbol

--- qlient/drawarea.cpp
+++ qlient/drawarea.cpp
@@ -10,6 +10,12 @@ DrawArea::DrawArea(Table *table) :
   table_{*table} {
 }
 
+const std::vector<bool>  DrawArea::shading_fill_passes_[3] = {
+  std::vector<bool>{true},        // filled
+  std::vector<bool>{true, false}, // stripped
+  std::vector<bool>{false}        // open
+};
+
 const QColor DrawArea::card_colors_[3] = {
   QColor{"#e21"}, // ~red
   QColor{"#382"}, // ~green
@@ -21,9 +27,9 @@ void DrawArea::paintEvent(QPaintEvent *event) {
   int w = width();
   int h = height();
   qDebug() << fmt::format("DrawArea::paintEvent w={}, h={}", w, h);
-  QPainter ell(this);
-  ell.setPen(QColor(0x22, 0xaa, 0x33));
-  ell.drawRect(0, 0, w, h);
+  QPainter painter(this);
+  painter.setPen(QColor(0x22, 0xaa, 0x33));
+  painter.drawRect(0, 0, w, h);
   const Game *game = table_.GetGame();
   if (game) {
     auto const &cards = game->cards_active_;
@@ -33,19 +39,33 @@ void DrawArea::paintEvent(QPaintEvent *event) {
     QColor white(0xff, 0xff, 0xff);
     for (size_t ai = 0; ai < cards.size(); ++ai) {
       const au2_t pos = golden_.GetCardPosition(ai);
-      ell.fillRect(pos[0], pos[1], card_size[0], card_size[1], white);
-      size_t card = cards[ai];
-      auto qr = std::div(card, 3);
-      unsigned shading = qr.rem;
-      qr = std::div(qr.quot, 3);
-      unsigned color = qr.rem;
-      unsigned symbol = qr.quot;
-      unsigned symbols = symbol + 1;
-      qDebug() << fmt::format(
-        "ai={} card={}, shading={} color={} symbols={}\n",
-        ai, card, shading, color, symbols);
+      QRect card_rect(pos[0], pos[1], card_size[0], card_size[1]);
+      DrawCard(painter, game->cards_active_[ai], card_rect);
     }
   }
+}
+
+void DrawArea::DrawCard(
+    QPainter &painter,
+    unsigned card,
+    const QRect &card_rect) {
+  static const QColor white(0xff, 0xff, 0xff);
+  painter.fillRect(card_rect, white);
+  auto qr = std::div(card, 3);
+  unsigned shading = qr.rem;
+  qr = std::div(qr.quot, 3);
+  unsigned color = qr.rem;
+  qr = std::div(qr.quot, 3);
+  unsigned symbol = qr.rem;
+  unsigned n_symbols = qr.quot + 1;
+  qDebug() << fmt::format(
+    "card={}, shading={} color={} symbol={}, n_symbols={}\n",
+    card, shading, color, symbol, n_symbols);
+  static std::array<void (DrawArea::*)(const QRect&, unsigned, unsigned), 3> 
+    draw_sym_funcs =
+      {&DrawArea::DrawDiamond, &DrawArea::DrawSquiggle, &DrawArea::DrawOval};
+  auto draw_symbol = draw_sym_funcs[symbol];
+  ((*this).*draw_symbol)(QRect(), shading, color);
 }
 
 void DrawArea::SetBrushes(unsigned card_height) {
@@ -58,5 +78,10 @@ void DrawArea::SetBrushes(unsigned card_height) {
 }
 
 void DrawArea::SetBrush(unsigned ci) {
+  QPixmap pattern(8, 2*stripe_height_);
+  pattern.fill(QColor(0xff, 0xff, 0xff));
+  QPainter painter(&pattern);
+  painter.fillRect(0, 0, 8, stripe_height_, card_colors_[ci]);
+  color_brushes_[ci] = QBrush(8, pattern);
 }
 
--- qlient/drawarea.h
+++ qlient/drawarea.h
@@ -1,9 +1,12 @@
 #pragma once
 
+#include <vector>
 #include <QWidget>
 #include "golden.h"
 
 class QPaintEvent;
+class QPainter;
+class QRect;
 class Table;
 
 class DrawArea : public QWidget {
@@ -12,8 +15,13 @@ class DrawArea : public QWidget {
  protected:
   void paintEvent(QPaintEvent *event) override;
  private:
+  void DrawCard(QPainter &painter, unsigned card, const QRect &card_rect);
+  void DrawDiamond(const QRect& symbol_rect, unsigned shading, unsigned color);
+  void DrawSquiggle(const QRect& symbol_rect, unsigned shading, unsigned color);
+  void DrawOval(const QRect& symbol_rect, unsigned shading, unsigned color);
   void SetBrushes(unsigned card_height);
   void SetBrush(unsigned ci);
+  static const std::vector<bool> shading_fill_passes_[3];
   static const QColor card_colors_[3]; // red, green, purple
   QBrush color_brushes_[3];
   unsigned stripe_height_{0};
