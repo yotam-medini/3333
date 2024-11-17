fa6850d5424b3badd06321992a1541b339e3aa38
Yotam Medini <yotam.medini@gmail.com>
2024-11-17 15:20:33 +0200

    ws_session add debug

--- server/ws_session.cc
+++ server/ws_session.cc
@@ -31,8 +31,8 @@ void WebSocketSession::send(const std::string &s) {
 
 void WebSocketSession::on_accept(error_code ec) {
   if (ec) {
-    std::cerr << fmt::format("{} {} failed, ec={}\n",
-      YMDHMS(), FuncName() << ec);
+    std::cerr << fmt::format("{} {} failed, ec=({}) {}\n",
+      YMDHMS(), FuncName(), ec.value(), ec.message());
   } else {
     read_next();
   }
@@ -48,8 +48,8 @@ void WebSocketSession::read_next() {
 
 void WebSocketSession::on_read(error_code ec, std::size_t) {
   if (ec) { 
-    std::cerr << fmt::format("{} {} failed, ec={}\n",
-      YMDHMS(), FuncName() << ec);
+    std::cerr << fmt::format("{} {} failed, ec=({}) {}\n",
+      YMDHMS(), FuncName(), ec.value(), ec.message());
     delete_me_();
   } else {
     // state_->send(beast::buffers_to_string(buffer_.data()));
@@ -71,8 +71,8 @@ void WebSocketSession::write_next() {
 void WebSocketSession::on_write(error_code ec, std::size_t)
 {
   if (ec) {
-    std::cerr << fmt::format("{} {} failed, ec={}\n",
-      YMDHMS(), FuncName() << ec);
+    std::cerr << fmt::format("{} {} failed, ec=({}) {}\n",
+      YMDHMS(), FuncName(), ec.value(), ec.message());
   } else {
     queue_.pop();
     if (!queue_.empty()) {
