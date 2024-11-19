93968bb740bb009a348ab1e4252b1b3d9c46f393
Yotam Medini <yotam.medini@gmail.com>
2024-11-19 14:53:05 +0200

    icons data for some icons

--- qlient/icondata.h
+++ qlient/icondata.h
@@ -4,3 +4,7 @@
 extern const QByteArray smile_data;
 extern const QByteArray frown_data;
 extern const QByteArray score_data;
+extern const QByteArray pick_data;
+extern const QByteArray players_data;
+extern const QByteArray found_data;
+extern const QByteArray deck_data;
